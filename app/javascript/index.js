eb45d964d95e324c06e95761f27678f0b0d26895
anthonylekan <anthonylekan25@gmail.com>
2018-01-12 14:20:16 -0500

    notebook.html UI interface updated
    Signed-off-by: anthonylekan <anthonylekan25@gmail.com>

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -4,10 +4,13 @@ $ = require('jquery');
 $(document).ready(function() {
     console.log("HELLO!");
 
+    $('#selectNotebook').removeClass('hidden');
+
 });
 
 // Creates a datalist with Id 'notebookList'
 function addLoadedNotebooks() {
+
 }
 
 $('body').on('click', function() {
